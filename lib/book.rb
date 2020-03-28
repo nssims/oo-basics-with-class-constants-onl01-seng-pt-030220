cd4fb281c822e86384a0aef4289882f4f6ea4590
Nicole Sims <nssims105@gmail.com>
2020-03-28 05:07:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,13 +7,12 @@ class Book
   def initialize(title)
     @title = title
   end
+  
+  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
   
- def genre=(genre)
-   @genre = genre
-   GENRES << genre
- end
+ 
 end
